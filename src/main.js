4028f221069889e70d98db255b0c3895e347e07f
icraft-nz <josh@icraft.co.nz>
2019-03-27 12:36:52 +1300

    sd

--- src/main.js
+++ src/main.js
@@ -5,8 +5,6 @@ import store from './store'
 import './registerServiceWorker'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
-
-// import '~vuetify/src/stylus/main'
 Vue.use(Vuetify)
 Vue.config.productionTip = false
 
